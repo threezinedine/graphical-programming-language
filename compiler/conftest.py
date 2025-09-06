2dccdf00424a239ea09fb82d0b66610b4fa0d528
threezinedine <threezinedine@gmail.com>
2025-09-06 09:19:50 +0700

    [refactor] split multiple parser testing

--- compiler/conftest.py
+++ compiler/conftest.py
@@ -1,5 +1,8 @@
 import os
 import sys
+from typing import Any
+import pytest
+from parser import Parser
 
 
 def pytest_configure():
@@ -7,3 +10,19 @@ def pytest_configure():
     sys.path.append(
         os.path.join(os.path.dirname(os.path.abspath(__file__)), "__tests__")
     )
+
+
+class ASTBuilder:
+    def __init__(self, content: str) -> None:
+        self._content = content
+
+    def Build(self) -> Parser:
+        parser = Parser(self._content)
+        parser.Program.Compress()
+        parser.Program.Parse()
+        return parser
+
+
+@pytest.fixture
+def astBuilder() -> Any:
+    return ASTBuilder
